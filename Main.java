ba0f09fc75599b93e7c622610d6919305b10b8d0
Montse <montseaweb@gmail.com>
2019-11-20 16:51:43 +0100

    Modificado Main

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main
      *  
      */
     public static void main(String[] args){
-        System.out.println("Hello world Hola mundo");
+        System.out.println("Hello world Hola");
 		System.out.println("Probando");
 		int a = 10;
 		
